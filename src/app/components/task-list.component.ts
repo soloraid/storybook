7f0108c98a7c12e71ebb48823fb6b145693d301c
Hashem Pourallahverdi <h.pourallahverdi@pnmco.info>
2021-10-12 12:42:39 +0330

    change template

--- src/app/components/task-list.component.ts
+++ src/app/components/task-list.component.ts
@@ -1,59 +1,36 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
 
 import { Task } from '../models/task.model';
+import { ArchiveTask, PinTask, TasksState } from '../state/task.state';
 
 @Component({
-    selector: 'app-task-list',
+    selector: 'app-pure-task-list',
     template: `
-       <div class="list-items">
-     <app-task
-       *ngFor="let task of tasksInOrder"
-       [task]="task"
-       (onArchiveTask)="onArchiveTask.emit($event)"
-       (onPinTask)="onPinTask.emit($event)"
-     >
-     </app-task>
-     <div *ngIf="tasksInOrder.length === 0 && !loading" class="wrapper-message">
-       <span class="icon-check"></span>
-       <div class="title-message">You have no tasks</div>
-       <div class="subtitle-message">Sit back and relax</div>
-     </div>
-     <div *ngIf="loading">
-       <div *ngFor="let i of [1, 2, 3, 4, 5, 6]" class="loading-item">
-         <span class="glow-checkbox"></span>
-         <span class="glow-text"> <span>Loading</span> <span>cool</span> <span>state</span> </span>
-       </div>
-     </div>
-   </div>
+      <app-pure-task-list
+      [tasks]="tasks$ | async"
+      (onArchiveTask)="archiveTask($event)"
+      (onPinTask)="pinTask($event)"
+    ></app-pure-task-list>
   `,
 })
 export class TaskListComponent {
-    /** The list of tasks */
-    //   @Input() tasks: Task[] = [];
+    @Select(TasksState.getAllTasks) tasks$: Observable<Task[]>;
+
+    constructor(private store: Store) { }
+
     /**
-     * @ignore
-     * Component property to define ordering of tasks
+     * Component method to trigger the archiveTask event
      */
-    tasksInOrder: Task[] = [];
+    archiveTask(id: string): void {
+        this.store.dispatch(new ArchiveTask(id));
+    }
 
-    /** Checks if it's in loading state */
-    @Input() loading = false;
-
-    /** Event to change the task to pinned */
-    // tslint:disable-next-line: no-output-on-prefix
-    @Output()
-    onPinTask = new EventEmitter<Event>();
-
-    /** Event to change the task to archived */
-    // tslint:disable-next-line: no-output-on-prefix
-    @Output()
-    onArchiveTask = new EventEmitter<Event>();
-
-    @Input()
-    set tasks(arr: Task[]) {
-        this.tasksInOrder = [
-            ...arr.filter(t => t.state === 'TASK_PINNED'),
-            ...arr.filter(t => t.state !== 'TASK_PINNED'),
-        ];
+    /**
+     * Component method to trigger the pinTask event
+     */
+    pinTask(id: string): void {
+        this.store.dispatch(new PinTask(id));
     }
 }
