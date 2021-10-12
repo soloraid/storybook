b72431741e098c3057cbbfe947397095718db768
Hashem Pourallahverdi <h.pourallahverdi@pnmco.info>
2021-10-12 10:46:00 +0330

    create tasklist component and story

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TaskComponent } from './components/task.component';
+import { TaskListComponent } from './components/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskComponent,
+    TaskListComponent,
   ],
   imports: [
     BrowserModule
