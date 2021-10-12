c6074dc08b0fa71cfb02f265c605d6bd49bd37e7
Hashem Pourallahverdi <h.pourallahverdi@pnmco.info>
2021-10-12 12:43:01 +0330

    create task module

---
+++ src/app/components/task.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgxsModule } from '@ngxs/store';
+
+import { TaskComponent } from './task.component';
+import { TasksState } from '../state/task.state';
+import { PureTaskListComponent } from './pure-task-list.component';
+import { TaskListComponent } from './task-list.component';
+@NgModule({
+    imports: [CommonModule, NgxsModule.forFeature([TasksState])],
+    exports: [TaskComponent, TaskListComponent],
+    declarations: [TaskComponent, TaskListComponent, PureTaskListComponent],
+    providers: [],
+})
+export class TaskModule { }
