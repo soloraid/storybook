aaac78ea352ef0a8909b4b1d53bde09b4a7bba4f
Hashem Pourallahverdi <h.pourallahverdi@pnmco.info>
2021-10-12 12:38:30 +0330

    add ngxs modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TaskComponent } from './components/task.component';
-import { TaskListComponent } from './components/task-list.component';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { TaskModule } from './components/task.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent,
-    TaskListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TaskModule,
+    NgxsModule.forRoot([]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
